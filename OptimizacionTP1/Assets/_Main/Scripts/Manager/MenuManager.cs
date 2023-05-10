61043f2afe901fe61bbcc8a6527ed878bae47e1e
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2023-05-10 13:23:39 -0300

    Added Main menu + UI

---
+++ OptimizacionTP1/Assets/_Main/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace _Main.Scripts.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private string sceneToLoad;
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button quitButton;
+        
+        private void Awake()
+        {
+            playButton.onClick.AddListener(OnPlayButtonClicked);            
+            quitButton.onClick.AddListener(OnQuitButtonClicked);
+            
+            playButton.Select();
+        }
+
+        private void OnPlayButtonClicked()
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+        private void OnQuitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
